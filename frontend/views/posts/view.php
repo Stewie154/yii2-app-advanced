efa53bbc41bcfe32bfae4503fbfa7ee7e7b741e0
Stewart <stewart@toru.digital>
2021-04-16 12:57:04 +0100

    Styles for view.php
    styles for individual blog posts

--- frontend/views/posts/view.php
+++ frontend/views/posts/view.php
@@ -1,13 +1,18 @@
 <h1><?=$model->Title?></h1>
 <br>
 <h3 id="by">BY: <?=$model->Author?></h3>
-<p><?=$model->Content?></p>
+<p class="content"><?=$model->Content?></p>
 <h2>Follow <?=$model->Author?>:</h2>
-<ul>
-    <li><a href="www.twitter.com">Twitter</a></li>
-    <li><a href="">Facebook</a></li>
-    <li><a href="">Instagram</a></li>
-</ul>
+<div class="author-info">
+    <img id="view-pic" src=<?=$model->picture?> alt="author">
+    <ul class="social-links">
+        <li><a href="https://www.google.co.uk/">Twitter</a></li>
+        <li><a href="https://www.google.co.uk/">Facebook</a></li>
+        <li><a href="https://www.google.co.uk/">Instagram</a></li>
+    </ul>
+</div>
+
+
 
 
 <a class="btn btn-primary" href="/posts/index">back</a>
